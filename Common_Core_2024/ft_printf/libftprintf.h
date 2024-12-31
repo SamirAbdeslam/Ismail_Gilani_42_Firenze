9c4e368ed4aef6f609043676f1cfbc32ba2f63aa
Samir Abdeslam <ilijuve@hotmail.it>
2024-12-31 14:09:10 +0100

    Inizio printf

---
+++ Common_Core_2024/ft_printf/libftprintf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igilani <igilani@student.42firenze.it>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/31 13:51:30 by igilani           #+#    #+#             */
+/*   Updated: 2024/12/31 13:53:39 by igilani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <libft.h>
+# include <stdarg.h>
+
+int ft_printf(const char *, ...);
+
+#endif
