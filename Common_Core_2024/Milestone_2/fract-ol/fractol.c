a5a1226b2ff633ab815b8509aa3a0254c21892d5
Samir <ilijuve@hotmail.it>
2025-02-10 17:08:14 +0100

    Creato l'algoritmo per il calcolo di mandelbroat

--- Common_Core_2024/Milestone_2/fract-ol/fractol.c
+++ Common_Core_2024/Milestone_2/fract-ol/fractol.c
@@ -6,50 +6,86 @@
 /*   By: igilani <igilani@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 17:15:58 by igilani           #+#    #+#             */
-/*   Updated: 2025/02/07 19:09:27 by igilani          ###   ########.fr       */
+/*   Updated: 2025/02/10 17:04:09 by igilani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int key_press(int keycode, void *param) {
-    (void)param;
-    if (keycode == XK_Escape)
-        exit(0);
-    return (0);
-}
+// int close_window(void *param)
+// {
+// 	(void)param;
+// 	exit(0);
+// }
 
-int close_window(void *param) {
-    (void)param;
-    exit(0);
-}
+// int	key_hook(int keycode, t_data data)
+// {
+// 	if (keycode == KEY_ESC)
+// 		close_window(&data.win);
+// 	// if (keycode == KEY_PLUS)
+// 	// 	data.max_iter += 10;
+// 	// if (keycode == KEY_MINUS)
+// 	// 	data.max_iter -= 10;
+// 	// render_fractal(data); // Funzione di ridisegno
+// 	return (0);
+// }
 
-int main()
+void	usage(int argc, char **argv)
 {
-	void	*mlx = mlx_init();
-	void	*win = mlx_new_window(mlx, WIDTH, HEIGHT, "Fractol");
-	void	*img = mlx_new_image(mlx, WIDTH, HEIGHT);
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-
-	char	*buffer = mlx_get_data_addr(img, &bits_per_pixel, &line_length, &endian);
+	if((2 == argc && !ft_strncmp(argv[1], "mandelbrot", 10)) || (4 == argc && !ft_strncmp(argv[1], "julia", 5)))
+	{
+		return;
+	}
+	else
+	{
+		ft_putstr_fd("Usage: ./fractol mandelbrot or julia 'value 1' 'value 2'\n", 2);
+		exit(EXIT_FAILURE);
+	}
+}
 	
-	int	y;
-	int	x;
-	int	offset;
-	x = 100;
-	y = 100;
-	offset = (y * line_length) + (x * 4);
+int main(int argc, char **argv)
+{
+	//usage(argc, argv);
+	// t_data	data;
+	// int		y;
+	// int		x;
+	// int		offset;
+	int		i = 0;
+	(void)argc;
+	t_complex z;
+	t_complex c; //punto specifico del frattale
+	double temp;
 
-	buffer[offset] = 0x00;
-	buffer[offset + 1] = 0x00;
-	buffer[offset + 2] = 0xFF;
+	z.r = 0;
+	z.i = 0;
 
-	mlx_put_image_to_window(mlx, win, img, 0, 0);
+	c.r = atof(argv[1]);
+	c.i = atof(argv[2]);
 
-    mlx_key_hook(win, key_press, NULL);
-    mlx_hook(win, 17, 0, close_window, NULL);
-	mlx_loop(mlx);
-	return(0);
+	while(i < ft_atoi(argv[3]))
+	{
+		temp = (z.r * z.r) - (z.i * z.i);
+		
+		z.i = 2 * z.r * z.i;
+		z.r = temp;
+
+		z.r += c.r;
+		z.i += c.i;
+
+		printf("Iterazione ->%d: Reale = %f, Immaginario = %f\n", i, z.r, z.i);
+		i++;
+	}
+
+	// data.mlx = mlx_init();
+	// data.win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "Fractol");
+	// data.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
+	// char *buffer = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
+
+	
+
+	// mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
+	// mlx_key_hook(data.win, key_hook, &data);
+	// mlx_hook(data.win, 17, 0, close_window, NULL);
+	// mlx_loop(data.mlx);
+	return (0);
 }
--- Common_Core_2024/Milestone_2/fract-ol/fractol.h
+++ Common_Core_2024/Milestone_2/fract-ol/fractol.h
@@ -6,7 +6,7 @@
 /*   By: igilani <igilani@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 17:13:15 by igilani           #+#    #+#             */
-/*   Updated: 2025/02/07 18:48:36 by igilani          ###   ########.fr       */
+/*   Updated: 2025/02/10 17:00:03 by igilani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,61 @@
 
 # define WIDTH 800
 # define HEIGHT 600
+# define KEY_ESC XK_Escape
+# define KEY_PLUS XK_plus
+# define KEY_MINUS XK_minus
+# define KEY_SCROLL_UP 4
+# define KEY_SCROLL_DOWN 5
+# define KEY_UP XK_Up
+# define KEY_DOWN XK_Down
+# define KEY_LEFT XK_Left
+# define KEY_RIGHT XK_Right
+# include "tha_supreme_lib/tha_supreme_lib.h"
 # include "minilibx-linux/mlx_int.h"
 # include "minilibx-linux/mlx.h"
+# include <math.h>
+
+typedef struct	s_complex {
+	double	r;
+	double	i;
+}		t_complex;
+
+typedef struct	s_keys {
+	int	w;
+	int	a;
+	int	s;
+	int	d;
+	int	left;
+	int	right;
+	int	up;
+	int	down;
+}		t_keys;
+
+typedef struct	s_data {
+	void	*mlx;       // Connessione principale a MinilibX
+	void	*win;       // Finestra grafica
+	void	*img;       // Immagine buffer per il rendering
+	char	*addr;      // Indirizzo dati immagine
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	
+	// Parametri del frattale
+	double	offset_x;   // Spostamento sull'asse X
+	double	offset_y;   // Spostamento sull'asse Y
+	double	zoom;       // Livello di zoom
+	int		max_iter;   // Massime iterazioni per precisione
+	
+	// Gestione colori
+	int		color_pattern;
+	int		color1;
+	int		color2;
+	
+	// Input utente
+	t_keys	keys;
+	int		is_julia;   // Flag per Julia set
+	double	mouse_x;    // Posizione X mouse
+	double	mouse_y;    // Posizione Y mouse
+}		t_data;
 
 #endif
