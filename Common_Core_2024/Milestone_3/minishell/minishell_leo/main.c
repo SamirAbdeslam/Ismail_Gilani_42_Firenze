5cc32ff25202645233a28366f8927060fe560937
Samir <ilijuve@hotmail.it>
2025-05-07 18:44:46 +0200

    implementazione di echo

--- Common_Core_2024/Milestone_3/minishell/minishell_leo/main.c
+++ Common_Core_2024/Milestone_3/minishell/minishell_leo/main.c
@@ -6,7 +6,7 @@
 /*   By: igilani <igilani@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 16:21:16 by lmenoni           #+#    #+#             */
-/*   Updated: 2025/05/06 10:46:04 by igilani          ###   ########.fr       */
+/*   Updated: 2025/05/07 16:39:40 by igilani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,10 +49,16 @@ void    tokenize_input(t_data *data)
     }
 }
 
+int define_input(t_data *data)
+{
+    if (!ft_strncmp(data->input, "echo", 4))
+        return (echo(data), 1);
+    return (1);
+}
+
 int main()
 {
     t_data  data;
-    
     data = (t_data){0};
     while (1)
     {
@@ -61,16 +67,17 @@ int main()
             break ;
         data.token = NULL;
         data.last_token = NULL;
-        tokenize_input(&data);
-        check_pipe(data.token);
-        print_tokens(data.token);
+        data.input_array = ft_split(data.input, ' ');
+        define_input(&data);
+        // tokenize_input(&data);
+        // print_tokens(data.token);
         if (!ft_strncmp(data.input, "exit", ft_strlen(data.input)))
         {
             free(data.input);
             break;
         }
         add_history(data.input);
-        ft_printf("Hai scritto: %s\n", data.input);
+        // ft_printf("Hai scritto: %s\n", data.input);
         free(data.input);
         free_token(data.token);
     }
--- Common_Core_2024/Milestone_3/minishell/minishell_leo/minishell.h
+++ Common_Core_2024/Milestone_3/minishell/minishell_leo/minishell.h
@@ -6,7 +6,7 @@
 /*   By: igilani <igilani@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 16:21:28 by lmenoni           #+#    #+#             */
-/*   Updated: 2025/05/05 16:11:27 by igilani          ###   ########.fr       */
+/*   Updated: 2025/05/07 16:14:13 by igilani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,7 @@ typedef struct  s_token
 typedef struct  s_data
 {
     char        *input;
+    char       **input_array;
     t_token     *token;
     t_token     *last_token;
 }               t_data;
@@ -91,4 +92,5 @@ char	*find_cmd_path(char *cmd, char **path_dirs);
 void	exec(char **argv, char **env, char **path, int cmd_index);
 void	heredoc(char **argv);
 int		open_file(char *file, int flags);
+void    echo(t_data *data);
 #endif
