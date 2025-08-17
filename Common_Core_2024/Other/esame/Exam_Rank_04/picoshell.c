9636ed2c4d10d17da661e6b23e92780158f542ab
Samir Abdeslam <ilijuve@hotmail.it>
2025-08-17 16:40:53 +0200

    Sistemati i subject di ex04

--- Common_Core_2024/Other/esame/Exam_Rank_04/picoshell.c
+++ Common_Core_2024/Other/esame/Exam_Rank_04/picoshell.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: igilani <igilani@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/14 17:12:57 by igilani           #+#    #+#             */
-/*   Updated: 2025/08/14 21:15:43 by igilani          ###   ########.fr       */
+/*   Created: 2025/08/16 17:57:14 by igilani           #+#    #+#             */
+/*   Updated: 2025/08/17 16:37:32 by igilani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,66 +14,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
-int ft_strcmp(char *s1, char *s2)
-{
-	while (*s1 && *s2 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
-	return(*(unsigned char *)s1 - *(unsigned char *)s2);
-	
-}
-
-int count_pipes(char **cmds)
+int	picoshell(char **cmds[])
 {
 	int i = 0;
-	int j = 0;
+	int fd[2];
+	int in_fd = -1;
+	pid_t pid;
 
 	while (cmds[i])
 	{
-		if (ft_strcmp(cmds[i], "|") == 0)
+		if (cmds[i + 1] && pipe(fd) == -1)
+			return(1);
+		pid = fork();
+		if (pid == -1)
+			return(1);
+		if (pid == 0)
 		{
-			printf("Trovato pipe a: %d\n", i);
-			j++;
+			if (in_fd != -1)
+			{
+				if (dup2(in_fd, STDIN_FILENO) == -1)
+					exit(1);
+				close(in_fd);
+			}
+			if (cmds[i + 1])
+			{
+				if(dup2(fd[1], STDOUT_FILENO) == -1)
+					exit(1);
+				close(fd[0]);
+				close(fd[1]);
+			}
+			execvp(cmds[i][0], cmds[i]);
+			exit(1);
 		}
-		if (ft_strcmp(cmds[i], "|") != 0)
+		if (in_fd != -1)
+			close(in_fd);
+		if (cmds[i + 1])
 		{
-			//controllo l'indice di inizio e fine del comando
-			printf("cmds are: %s in: %d\n",cmds[i], i);
+			close(fd[1]);
+			in_fd = fd[0];
 		}
 		i++;
 	}
-	return(j);
-}
-
-// void save_cmd(char **cmds, int n_cmds)
-// {
-// 	int i = 0;
-// 	int j = 0;
-// 	char *args[n_cmds];
-
-// 	while (cmds[i])
-// 	{
-// 		if (ft_strcmp(cmds[i], "|") != 0)
-// 			args[j] = cmds[i];
-// 		i++;
-// 	}
-// }
-
-int	picoshell(char *cmds[])
-{
-	// int n_cmds = count_pipes + 1;
-	printf("pipes are: %d", count_pipes(cmds));
-	// save_cmd(cmds, n_cmds);
+	while(wait(NULL) > 0)
+		;
 	return(0);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	(void)argc;
-	argv++;
-	picoshell(argv);
-	return(0);
+	int	cmds_size = 1;
+	for (int i = 1; i < argc; i++)
+	{
+		if (!strcmp(argv[i], "|"))
+			cmds_size++;
+	}
+	char ***cmds = calloc(cmds_size + 1, sizeof(char **));
+	if (!cmds)
+	{
+		dprintf(2, "Malloc error: %m\n");
+		return 1;
+	}
+	cmds[0] = argv + 1;
+	int cmds_i = 1;
+	for (int i = 1; i < argc; i++)
+	{
+		if (!strcmp(argv[i], "|"))
+		{
+			cmds[cmds_i] = argv + i + 1;
+			argv[i] = NULL;
+			cmds_i++;
+		}
+	}
+	int ret = picoshell(cmds);
+	if (ret)
+		perror("picoshell");
+	free(cmds);
+	return ret;
 }
