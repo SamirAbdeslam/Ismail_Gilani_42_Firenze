103652b87eb1512326ce3ad5dcf0d02fc629a999
Samir Abdeslam <ilijuve@hotmail.it>
2025-08-27 19:14:04 +0200

    picoshell ha rotto il cazzo

---
+++ Common_Core_2024/Other/esame/Exam_Rank_04/Livello 1/no_help_2/picoshell.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   picoshell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igilani <igilani@student.42firenze.it>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 18:32:00 by igilani           #+#    #+#             */
+/*   Updated: 2025/08/27 19:13:22 by igilani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//Allowed functions:	close, fork, wait, exit, execvp, dup2, pipe
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int	picoshell(char **cmds[])
+{
+	
+}
