ddcf2479685499fba5ef8810c9a02087caf60400
Samir Abdeslam <ilijuve@hotmail.it>
2025-08-28 14:19:42 +0200

    Non so ancora fare pico

--- Common_Core_2024/Other/esame/Exam_Rank_04/Livello 1/no_help_2/picoshell.c
+++ Common_Core_2024/Other/esame/Exam_Rank_04/Livello 1/no_help_2/picoshell.c
@@ -6,7 +6,7 @@
 /*   By: igilani <igilani@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 18:32:00 by igilani           #+#    #+#             */
-/*   Updated: 2025/08/27 19:13:22 by igilani          ###   ########.fr       */
+/*   Updated: 2025/08/28 14:17:37 by igilani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,57 @@
 
 int	picoshell(char **cmds[])
 {
-	
+	int fd[2];
+	int in_fd = -1;
+	pid_t pid;
+	int i = 0;
+
+	while (cmds[i])
+	{
+		if (cmds[i + 1])
+		{
+			if (pipe(fd) < 0)
+				return (1);
+		}
+		pid = fork();
+
+		if (pid < 0)
+		{
+			if (in_fd != -1)
+			{
+				close(in_fd);
+				close(fd[0]);
+				close(fd[1]);
+				return (1);
+			}
+		}
+		if (pid == 0)
+		{
+			if (in_fd != -1)
+			{
+				dup2(in_fd, STDIN_FILENO);
+				//gestione errori
+				close(in_fd);
+			}
+			if (cmds[i + 1])
+			{
+				dup2(fd[1], STDOUT_FILENO);
+				close(fd[1]);
+				close(fd[0]);
+			}
+			execvp(cmds[i][0], cmds[i]);
+			exit (1);
+		}
+		if (in_fd != -1)
+			close(in_fd);
+		if (cmds[i + 1])
+		{
+			close(fd[1]);
+			in_fd = fd[0];
+		}
+		i++;
+	}
+	while(wait(NULL) > 0)
+		;
+	return(0);
 }
