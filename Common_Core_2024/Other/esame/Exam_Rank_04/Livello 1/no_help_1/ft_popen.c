91cac009bc7c093f3828428265e3bd13dfe42c01
Samir Abdeslam <ilijuve@hotmail.it>
2025-08-29 19:29:27 +0200

    sempre problemi con pico senza pensare al livello 2

--- Common_Core_2024/Other/esame/Exam_Rank_04/Livello 1/no_help_1/ft_popen.c
+++ Common_Core_2024/Other/esame/Exam_Rank_04/Livello 1/no_help_1/ft_popen.c
@@ -1,98 +1,82 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_popen_nohelp.c                                  :+:      :+:    :+:   */
+/*   ft_popen.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: igilani <igilani@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/26 15:15:53 by igilani           #+#    #+#             */
-/*   Updated: 2025/08/26 16:34:26 by igilani          ###   ########.fr       */
+/*   Created: 2025/08/29 12:41:18 by igilani           #+#    #+#             */
+/*   Updated: 2025/08/29 12:57:29 by igilani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//pipe, fork, dup2, execvp, close, exit
-
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdbool.h>
 #include <sys/types.h>
-#include "./my_libft/libft.h"
 
-int parsing(const char *file, char *const argv[], char type)
+bool parsing(const char *file, char *const argv[], char type)
 {
-	if (type != 'r' && type != 'w')
-		return (-1);
-	else if (!file)
-		return (-1);
-	else if (!argv)
-		return (-1);
+	if (file == NULL)
+		return (false);
+	else if (argv[0] == NULL)
+		return (false);
+	else if (type != 'r' && type != 'w')
+		return (false);
 	else
-		return (1);
-}
-
-void child(const char *file, char *const argv[], char type, int *fd)
-{
-	if (type == 'r')
-	{
-		if (dup2(fd[1], 1) == -1)
-		{
-			close(fd[0]);
-			close(fd[1]);
-			exit(-1);
-		}
-		close(fd[0]);
-		close(fd[1]);
-	}
-	else
-	{
-		if (dup2(fd[0], 0) == -1)
-		{
-			close(fd[1]);
-			close(fd[0]);
-			exit(-1);
-		}
-		close(fd[1]);
-		close(fd[0]);
-	}
-	execvp(file, argv);
-	exit(-1);
+		return (true);
 }
 
 int	ft_popen(const char *file, char *const argv[], char type)
 {
+	if (parsing(file, argv, type) == false)
+		return (-1);
 	int fd[2];
 	pid_t pid;
-	
-	if (parsing(file, argv, type) == -1)
-		return (-1);
-	if (pipe(fd) == -1)
+	if (pipe(fd) < 0)
 		return (-1);
 	pid = fork();
-	if (pid == -1)
-	{
-		close(fd[0]);
-		close(fd[1]);
+	if (pid < 0)
 		return (-1);
-	}
 	if (pid == 0)
-		child(file, argv, type, fd);
-	if (type == 'r')
 	{
-		close(fd[1]);
-		return(fd[0]);
+		if (type == 'r')
+		{
+			if (dup2(fd[1], 1) < 0)
+			{
+				close(fd[0]);
+				close(fd[1]);
+				exit (-1);
+			}
+			close(fd[0]);
+			close(fd[1]);
+		}
+		else
+		{
+			if (dup2(fd[0], 0) < 0)
+			{
+				close(fd[0]);
+				close(fd[1]);
+				exit (-1);
+			}
+			close(fd[0]);
+			close(fd[1]);
+		}
+		execvp(file, argv);
+		exit (-1);
 	}
 	else
 	{
-		close(fd[0]);
-		return(fd[1]);
+		if (type == 'r')
+		{
+			close(fd[1]);
+			return(fd[0]);
+		}
+		else
+		{
+			close(fd[0]);
+			return(fd[1]);
+		}
 	}
-}
-
-int	main()
-{
-	int	fd = ft_popen("ls", (char *const []){"ls", NULL}, 'r');
-	char	*line;
-	while ((line = get_next_line(fd)))
-		ft_putstr_fd(line, fd);
-	return (0);
 }
--- Common_Core_2024/Other/esame/Exam_Rank_04/Livello 1/no_help_1/sandbox.c
+++ Common_Core_2024/Other/esame/Exam_Rank_04/Livello 1/no_help_1/sandbox.c
@@ -1,155 +1,72 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sandbox_nohelp.c                                   :+:      :+:    :+:   */
+/*   sandbox.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: igilani <igilani@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/27 14:35:05 by igilani           #+#    #+#             */
-/*   Updated: 2025/08/27 17:26:04 by igilani          ###   ########.fr       */
+/*   Created: 2025/08/29 17:18:22 by igilani           #+#    #+#             */
+/*   Updated: 2025/08/29 18:48:49 by igilani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-Allowed functions	:	fork, waitpid, exit, alarm, sigaction, kill, printf, strsignal, 
-						errno, sigaddset, sigemptyset, sigfillset, sigdelset, sigismember
-*/
-
-#include <unistd.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
+#include <unistd.h>
 #include <sys/types.h>
-#include <sys/time.h>
+#include <sys/wait.h>
 #include <signal.h>
 #include <string.h>
-#include <stdbool.h>
-#include <errno.h>
-
-bool parsing(void (*f)(void), unsigned int timeout)
-{
-	if (f == NULL)
-		return (false);
-	else if (timeout < 0)
-		return (false);
-	else
-		return (true);
-}
 
 int	sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 {
-	if (parsing(f, timeout) == false)
+	if (f == NULL)
 		return (-1);
-
-	int status = 0;
 	pid_t pid;
-	
-	pid = fork();
+	int status;
 
+	pid = fork();
 	if (pid < 0)
-		exit(-1);
+		return (-1);
 	if (pid == 0)
 	{
-		alarm(timeout); //prima allarme POI FUNZIONE
+		alarm(timeout);
 		f();
-		exit(0);
+		exit (0);
 	}
 	else
 	{
 		waitpid(pid, &status, 0);
-		
 		if (WIFEXITED(status))
 		{
 			if (WEXITSTATUS(status) == 0)
 			{
 				if (verbose)
 					printf("Nice function!\n");
+				return (1);
 			}
 			else
 			{
 				if (verbose)
 					printf("Bad function: exited with code %d\n", WEXITSTATUS(status));
+				return (0);
 			}
-			return (WEXITSTATUS(status));
 		}
-		else if (WIFSIGNALED(status))
+		if (WIFSIGNALED(status))
 		{
 			if (WTERMSIG(status) == SIGALRM)
 			{
 				if (verbose)
-					printf("Bad function: timed out after %d seconds\n", timeout);
+					printf("Bad function: timed out after %u seconds\n", timeout);
 			}
 			else
 			{
 				if (verbose)
-					printf("Bad function: %s\n", strsignal(WTERMSIG(status))); //bisogna passare il TERMSIGN a strsignal
+					printf("Bad function: %s\n", strsignal(WTERMSIG(status)));
 			}
-			return (WTERMSIG(status));
-		}
-		else
-		{
-			
-			return (WEXITSTATUS(status));
+			return (0);
 		}
 	}
 	return (-1);
-}
-
-/* Nice function */
-void	nice_function(void)
-{
-	printf("\n1. Hello from nice_function!\n");
-}
-
-/* Causes segmentation fault */
-void	segfault_function(void)
-{
-	printf("\n2. Segfault function\n");
-	int	*ptr = NULL;
-	*ptr = 42;
-}
-
-/* Runs forever */
-void	infinite_loop(void)
-{
-	printf("\n3. Infinite loop function\n");
-	while (1) {}
-}
-
-/* Exits with error code 42 */
-void	exit_with_error(void)
-{
-	printf("\n4. Exit_with_error function\n");
-	exit(42);
-}
-
-void segfault_function_2(void) 
-{
-	printf("\n5. Segfault function\n");
-	*(volatile char *)0 = 0;
-}
-
-void abnormal_termination(void) 
-{
-	printf("\n6. Abnormal termination function\n");
-	abort();
-}
-
-int	main()
-{
-	printf("----- SANDBOX VERBOSE -----\n");
-	sandbox(nice_function, 5, true);
-	sandbox(segfault_function, 5, true);
-	sandbox(infinite_loop, 2, true);
-	sandbox(exit_with_error, 5, true);
-	sandbox(segfault_function_2, 5, true);
-	sandbox(abnormal_termination, 5, true);
-	printf("\n----- SANDBOX NON VERBOSE -----\n");
-	printf("Return code: %d\n", sandbox(nice_function, 5, false));
-	printf("Return code: %d\n", sandbox(segfault_function, 5, false));
-	printf("Return code: %d\n", sandbox(infinite_loop, 2, false));
-	printf("Return code: %d\n", sandbox(exit_with_error, 5, false));
-	printf("Return code: %d\n", sandbox(segfault_function_2, 5, false));
-	printf("Return code: %d\n", sandbox(abnormal_termination, 5, false));
-	return (0);
 }
